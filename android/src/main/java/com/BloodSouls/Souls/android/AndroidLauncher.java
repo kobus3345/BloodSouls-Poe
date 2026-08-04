82b3999d4aaad18d4eb69a7ebb8943219c92d4c3
kobus3345 <kobus3345@gmail.com>
2026-08-04 11:37:12 +0200

    here i added the first test map i did it a couple of days ago but the push failed lets see if this works now

--- android/src/main/java/com/BloodSouls/Souls/android/AndroidLauncher.java
+++ android/src/main/java/com/BloodSouls/Souls/android/AndroidLauncher.java
@@ -2,9 +2,12 @@ package com.BloodSouls.Souls.android;
 //This file was auto generated when i created the project file with GDX
 import android.os.Bundle;
 
+import com.BloodSouls.Souls.BloodSoulsGame;
+import com.BloodSouls.Souls.MapScreen;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.BloodSouls.Souls.MainGame;
+import com.BloodSouls.Souls.MapScreen;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -15,6 +18,6 @@ public class AndroidLauncher extends AndroidApplication {
         configuration.useImmersiveMode = true;
         //I had to come in there though and change the file that loaded when the program started
         // The original line was initialize(new Main(), configuration);
-        initialize(new MainGame(), configuration);
+        initialize(new BloodSoulsGame(), configuration);
     }
 }
